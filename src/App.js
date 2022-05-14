18a2b9b4e719c1b339827531667df62f390f3106
Ahsan <achsan.ajr@gmail.com>
2022-05-14 08:58:06 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const links = [
+    {
+      link: "https://shope.ee/6KPdimoIhU",
+      judul: "Bel Anti Air Wireless",
+    },
+    {
+      link: "https://shope.ee/7KIBEZUzBY",
+      judul: "Penutup Stop Kontak",
+    },
+    {
+      link: "https://shope.ee/8UU8dLNzQ9",
+      judul: "Penjepit Kabel di Dinding",
+    },
+    {
+      link: "https://shope.ee/6f2USU4dZh",
+      judul: "Penutup Kemasan Agar Tidak Tumpah",
+    },
+    {
+      link: "https://shope.ee/VRr7TSu1I",
+      judul: "Pengupas Buah Apel / Pir",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex w-[500px] justify-center items-center flex-col my-10 border shadow-lg mx-auto">
+      <h2 className="max-w-xl rounded p-1 m-3 text-2xl">
+        Link Teman Belanjamu
+      </h2>
+      {links.map((link, index) => (
+        <div
+          key={index}
+          className="container max-w-md border border-black rounded p-3 m-1 bg-orange-300 text-black hover:bg-orange-400 hover:text-white"
         >
-          Learn React
-        </a>
-      </header>
+          <a href={link.link} target="_blank">
+            <h2>
+              {index + 1}. {link.judul}
+            </h2>
+          </a>
+        </div>
+      ))}
+      <div className="m-3">
+        <p className="text-sm italic">Created by A-san</p>
+      </div>
     </div>
   );
 }
